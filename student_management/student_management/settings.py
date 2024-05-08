2ca676bf55c166f01c9135d673f2f176d1441fa2
cobyyt <cobyttung@gmail.com>
2024-05-08 21:29:51 +0800

    second commit

--- student_management/student_management/settings.py
+++ student_management/student_management/settings.py
@@ -12,13 +12,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2q(ohf)#)ym@&i7lu5$8x%k9f$t#0fyubs(fw65h_=d!u5ea)p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
